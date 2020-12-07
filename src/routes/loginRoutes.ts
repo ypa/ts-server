a19d6ca81e4dad68527539b6666a9b46bf2bc158
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-07 07:16:06 -0800

    Add /login route handler that renders login form

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,11 +2,19 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
   res.send(`
-    <div>
-      <h1>Hi there!</h1>
-    </div>
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
   `);
 });
 
