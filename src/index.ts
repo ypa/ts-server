858ac887b6425f808ce470e7c24ba72062cf5b5b
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-08 07:31:53 -0800

    Wire up cookieSession middleware to express app

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['asfasdfasdf'] }));
 app.use(router);
 
 app.listen(3000, () => {
