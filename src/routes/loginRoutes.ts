ba934b8533cc80c7b4cb442cf762ec98bb021068
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-08 07:20:30 -0800

    Add RequestWithBody interface with type annotations to augment the type definition files

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,9 @@
 import { Router, Request, Response } from 'express';
 
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined }
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
