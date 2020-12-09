af408a0f6cfbe4528b2c7353bf5121bf1596004d
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-09 06:55:00 -0800

    Add logout handler

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -53,4 +53,9 @@ router.get('/', (req: Request, res: Response) => {
   }
 });
 
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined
+  res.redirect('/');
+});
+
 export { router };
