7a4d6937b6bbe11ab195406bffe7ecad02453066
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-12 09:16:32 -0800

    Add Controller with router prefix and use it in express app

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { get } from './decorators/routes';
+import { controller } from './decorators/controller';
 
+@controller('/auth')
 class LoginController {
 
   @get('/login')
