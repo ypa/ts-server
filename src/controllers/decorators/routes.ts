4cc11edec706459a0ce75a9fba276102127d019c
Yan P Aung <yan.pye.aung@gmail.com>
2020-12-13 07:45:24 -0800

    Wrap with routeBinder for different http methods

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,7 +1,17 @@
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
-  };
+function routeBinder(method: string) {
+  return function(path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
+  }
 }
+
+export const get = routeBinder('get');
+export const put = routeBinder('put');
+export const post = routeBinder('post');
+export const del = routeBinder('del');
+export const patch = routeBinder('patch');
+
